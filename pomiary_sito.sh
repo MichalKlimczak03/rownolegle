cd964cc0a1045c93716f7a9c569df3c31997f57c
MichalKlimczak03 <161569101+MichalKlimczak03@users.noreply.github.com>
2026-04-29 13:03:21 +0200

    Update pomiary_sito.sh

--- pomiary_sito.sh
+++ pomiary_sito.sh
@@ -23,20 +23,29 @@ do
     # --- SEQ ---
     $GENG -c $N $K:$K | head -n $SIZE | $SEQ -q > /dev/null 2> tmp_seq.txt
 
-    TIME=$(grep "czas_s" tmp_seq.txt | awk -F': ' '{print $2}')
-    FOUND=$(grep "znalezione_grafy_calkowite" tmp_seq.txt | awk -F': ' '{print $2}')
+    TIME=$(grep "czas_s" tmp_seq.txt | cut -d':' -f2 | tr -d ' ')
+    FOUND=$(grep "znalezione_grafy_calkowite" tmp_seq.txt | cut -d':' -f2 | tr -d ' ')
 
     echo "seq,$SIZE,$TIME,$FOUND" >> $OUT
 
     # --- OMP ---
     $GENG -c $N $K:$K | head -n $SIZE | $OMP $OMP_THREADS $BATCH_SIZE -q > /dev/null 2> tmp_omp.txt
 
-    TIME=$(grep "czas_s" tmp_omp.txt | awk -F': ' '{print $2}')
-    FOUND=$(grep "znalezione_grafy_calkowite" tmp_omp.txt | awk -F': ' '{print $2}')
+    TIME=$(grep "czas_s" tmp_omp.txt | cut -d':' -f2 | tr -d ' ')
+    FOUND=$(grep "znalezione_grafy_calkowite" tmp_omp.txt | cut -d':' -f2 | tr -d ' ')
 
     echo "openmp,$SIZE,$TIME,$FOUND" >> $OUT
 done
 
+echo ""
+echo "DEBUG (ostatni pomiar):"
+echo "SEQ:"
+cat tmp_seq.txt
+echo ""
+echo "OMP:"
+cat tmp_omp.txt
+
 rm -f tmp_seq.txt tmp_omp.txt
 
+echo ""
 echo "Gotowe. Wyniki zapisane w $OUT"
